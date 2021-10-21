1f8615299544c3720007d3d2d4f009520178e019
aisu-programming <aisu.programming@gmail.com>
2021-10-21 20:46:57 +0800

    Update

--- detect.py
+++ detect.py
@@ -5,7 +5,7 @@ from threading import Thread
 from model.simple_CNN import SimpleCNN
 
 
-def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
+def letterbox(img, new_shape, color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
     # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
     shape = img.shape[:2]  # current shape [height, width]
     if isinstance(new_shape, int):
@@ -39,27 +39,30 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
 
 
 class LoadStreams:  # multiple IP or RTSP cameras
-    def __init__(self, img_size=1280):
+    def __init__(self, img_size=(480, 640)):
+        self.mode = 'images'
         self.img_size = img_size
-        self.img = None
+
         sources = ['0']
+        n = len(sources)
+        self.imgs = [None] * n
         self.sources = sources
         for i, s in enumerate(sources):
             # Start the thread to read frames from the video stream
-            print('%g/%g: %s... ' % (i + 1, 0, s), end='')
-            cap = cv2.VideoCapture(0)
+            print('%g/%g: %s... ' % (i + 1, n, s), end='')
+            cap = cv2.VideoCapture(0 if s == '0' else s)
             assert cap.isOpened(), 'Failed to open %s' % s
             w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
             fps = cap.get(cv2.CAP_PROP_FPS) % 100
-            _, self.img = cap.read()  # guarantee first frame
+            _, self.imgs[i] = cap.read()  # guarantee first frame
             thread = Thread(target=self.update, args=([i, cap]), daemon=True)
             print(' success (%gx%g at %.2f FPS).' % (w, h, fps))
             thread.start()
         print('')  # newline
 
         # check for common shapes
-        s = np.stack([letterbox(self.img, new_shape=self.img_size)[0].shape], 0)  # inference shapes
+        s = np.stack([letterbox(x, new_shape=self.img_size)[0].shape for x in self.imgs], 0)  # inference shapes
         self.rect = np.unique(s, axis=0).shape[0] == 1  # rect inference if all shapes equal
         if not self.rect:
             print('WARNING: Different stream shapes detected. For optimal performance supply similarly-shaped streams.')
@@ -69,10 +72,10 @@ class LoadStreams:  # multiple IP or RTSP cameras
         n = 0
         while cap.isOpened():
             n += 1
-            # _, self.img = cap.read()
+            # _, self.imgs[index] = cap.read()
             cap.grab()
             if n == 4:  # read every 4th frame
-                _, self.img = cap.retrieve()
+                _, self.imgs[index] = cap.retrieve()
                 n = 0
             time.sleep(0.01)  # wait time
 
@@ -82,22 +85,22 @@ class LoadStreams:  # multiple IP or RTSP cameras
 
     def __next__(self):
         self.count += 1
-        img0 = self.img.copy()
+        img0 = self.imgs.copy()
         if cv2.waitKey(1) == ord('q'):  # q to quit
             cv2.destroyAllWindows()
             raise StopIteration
 
         # Letterbox
-        img = [letterbox(img0, new_shape=self.img_size, auto=self.rect)[0]]
+        img = [letterbox(x, new_shape=self.img_size, auto=self.rect)[0] for x in img0]
 
         # Stack
         img = np.stack(img, 0)
 
         # Convert
-        img = img[:, :, :, ::-1].transpose(0, 3, 1, 2)  # BGR to RGB, to bsx3x416x416
+        # img = img[:, :, :, ::-1].transpose(0, 3, 1, 2)  # BGR to RGB, to bsx3x416x416
         img = np.ascontiguousarray(img)
 
-        return self.sources, img, [img0], None
+        return self.sources, img, img0, None
 
     def __len__(self):
         return 0
@@ -112,11 +115,13 @@ if __name__ == "__main__":
     t0 = time.time()
     for path, img, im0s, vid_cap in LoadStreams():
 
+        # time.sleep(1)
+
         t1 = time.time()
 
         img = np.array(img, dtype=np.float32)
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        img = np.expand_dims(img, axis=0)
+        # img = np.expand_dims(img, axis=0)
 
         # Prediction
         t2 = time.time()
